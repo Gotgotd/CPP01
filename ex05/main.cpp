1d32d3228ec9a6df7cfc73f98a25cab8d0d2e431
Gautier Daigneau <gdaignea@c3r2p5.42perpignan.fr>
2024-08-20 17:10:43 +0200

    adjusted main.cpp

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gdaignea <gdaignea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 16:14:14 by gdaignea          #+#    #+#             */
-/*   Updated: 2024/08/20 16:29:03 by gdaignea         ###   ########.fr       */
+/*   Updated: 2024/08/20 17:10:11 by gdaignea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@ int	main() {
 	Harl	harl;
 
 	harl.complain("DEBUG");
+	std::cout << std::endl;
 	harl.complain("INFO");
+	std::cout << std::endl;
 	harl.complain("WARNING");
+	std::cout << std::endl;
 	harl.complain("ERROR");
+	std::cout << std::endl;
 	harl.complain("FINE");
 }
